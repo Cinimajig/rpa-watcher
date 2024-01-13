f48f967aa3ec8adcf125da068bfc1f8fc2915498
Cinimajig <jacob.mogensen5@gmail.com>
2024-01-13 13:15:58 +0100

    todo server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    let cfg = load_config();
+
+    rocket::build()
+    .configure(&cfg)
+    .mount("/", routes![index])
+}
+
+fn load_config() -> rocket::Config {
+    let mut config = {
+        #[cfg(debug_assertions)]
+        return rocket::Config::debug_default();
+        #[cfg(not(debug_assertions))]
+        return rocket::Config::release_default();
+    };
+
+    todo!()
+
+
+    config
 }
