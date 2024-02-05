8b5d5c24f6b1ca823651633b6cc4df9dc66618a5
Cinimajig <jacob.mogensen5@gmail.com>
2024-02-05 14:32:16 +0100

    Inital html view

--- wwwroot/view/script.js
+++ wwwroot/view/script.js
@@ -1,1 +1,22 @@
+const timeoutSeconds = 7;
 const rpaView = document.querySelector('div#rpa-view');
+
+let rpaData = [];
+let failedRpaData = [];
+
+setTimeout(buildRpaConvas, timeoutSeconds * 1000);
+
+const buildRpaConvas = async () => {
+    rpaData = await getRpaData();
+    failedRpaData = await getFailedRpaData();
+
+    const template = document.querySelector('div.rpa-info');
+}
+
+const getRpaData = async () => {
+
+}
+
+const getFailedRpaData = async () => {
+
+}
