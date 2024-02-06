8a4e617b837173e1c1a759e988f40ce188c56b44
Cinimajig <jacob.mogensen5@gmail.com>
2024-02-06 22:41:32 +0100

    template cloning

--- wwwroot/view/script.js
+++ wwwroot/view/script.js
@@ -1,22 +1,69 @@
 const timeoutSeconds = 7;
 const rpaView = document.querySelector('div#rpa-view');
 
-let rpaData = [];
-let failedRpaData = [];
+let rpaData = new Map();
+let failedRpaData = new Map();
 
-setTimeout(buildRpaConvas, timeoutSeconds * 1000);
+setTimeout(() => {
+    buildRpaConvas().catch((err) => {
+        clearCanvas();
+        console.error(err);
+    })
+}, timeoutSeconds * 1000);
 
 const buildRpaConvas = async () => {
-    rpaData = await getRpaData();
-    failedRpaData = await getFailedRpaData();
+    let data = await getRpaData();
+    let failed = await getFailedRpaData();
 
-    const template = document.querySelector('div.rpa-info');
+    rpaData.clear();
+    for (let i = 0; i < data.length; i++) {
+        rpaData.set(data[i].instance, data[i]);
+    }
+
+    for (let el of document.querySelectorAll('div.rpa-info')) {
+        let attr = el.getAttribute('data-ref');
+        if (!iteratorIncludes(attr, rpaData.keys())) {
+            document.querySelector(`div.rpa-info[data-ref="${attr}"`).remove();
+        }
+    }
+
+    for (let rpa of rpaData.entries()) {
+        const template = document.querySelector('template.rpa-info').content.cloneNode(true);
+        const div = template.querySelector('div.rpa-info');
+
+        div.setAttribute('data-ref', rpa[0]);
+        const engine = div.querySelector('div.engine');
+        const hostname = div.querySelector('div.hostname');
+        const env = div.querySelector('div.env');
+        const instance = div.querySelector('div.instance');
+        const runInfo = div.querySelector('div.run-info');
+
+        engine.innerText = rpaData;
+    }
+}
+
+const getRpaRunIds = () => {
+    rpaData.keys()
 }
 
 const getRpaData = async () => {
-
+    return await fetch('/api/getrpa')
 }
 
 const getFailedRpaData = async () => {
 
+}
+
+const clearCanvas = () => {
+    rpaData.clear();
+    failedRpaData.clear();
+    rpaView.innerHTML = '';
+}
+
+const iteratorIncludes = (item, iter) => {
+    for (let el in iter) {
+        if (el === item) return true
+    }
+
+    return false;
 }
