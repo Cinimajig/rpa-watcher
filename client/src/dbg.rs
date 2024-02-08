e1042e2be24c80f2df66e4ca7a306a12a77b3389
Cinimajig <jacob.mogensen5@gmail.com>
2024-02-08 21:25:58 +0100

    dbg_output

--- client/src/dbg.rs
+++ client/src/dbg.rs
@@ -2,6 +2,10 @@ use std::{fmt, iter};
 use windows::{core::PCWSTR, Win32::System::Diagnostics::Debug::OutputDebugStringW};
 
 pub fn dbg_output(text: impl fmt::Display) {
+    #[cfg(debug_assertions)]
+    println!("{text}");
+
+    #[cfg(not(debug_assertions))]
     unsafe {
         let dbg: Box<[u16]> = text.to_string().encode_utf16().chain(iter::once(0)).collect();
         OutputDebugStringW(PCWSTR::from_raw(dbg.as_ptr()));
