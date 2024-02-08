d2a28a15c974c0b411299532c5f98951167f407a
Cinimajig <jacob.mogensen5@gmail.com>
2024-02-08 21:26:51 +0100

    removed #[cfg(not(debug_assertions))]

--- client/src/dbg.rs
+++ client/src/dbg.rs
@@ -5,7 +5,6 @@ pub fn dbg_output(text: impl fmt::Display) {
     #[cfg(debug_assertions)]
     println!("{text}");
 
-    #[cfg(not(debug_assertions))]
     unsafe {
         let dbg: Box<[u16]> = text.to_string().encode_utf16().chain(iter::once(0)).collect();
         OutputDebugStringW(PCWSTR::from_raw(dbg.as_ptr()));
