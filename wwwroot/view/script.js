359a69ea31214f7b19ec52bc06de4ae99e25731d
Cinimajig <jacob.mogensen5@gmail.com>
2024-03-01 23:39:56 +0100

    RPA-Watcher www

--- wwwroot/view/script.js
+++ wwwroot/view/script.js
@@ -1,41 +1,52 @@
 const timeoutSeconds = 7;
-const rpaView = document.querySelector('div#rpa-view');
+const rpaView = document.querySelector('#rpa-view');
 
 let rpaData = new Map();
 let failedRpaData = new Map();
 
-const buildRpaConvas = async () => {
+const buildRpaConvas = async (clear) => {
     let data = await getRpaData();
     let failed = await getFailedRpaData();
 
-    rpaData.clear();
+    if (clear) {
+        rpaData.clear();
+    }
     for (let i = 0; i < data.length; i++) {
         rpaData.set(data[i].instance, data[i]);
     }
 
-    for (let el of document.querySelectorAll('div.rpa-info')) {
+    for (let el of document.querySelectorAll('.tr.rpa-info')) {
         let attr = el.getAttribute('data-ref');
         if (!iteratorIncludes(attr, rpaData.keys())) {
-            document.querySelector(`div.rpa-info[data-ref="${attr}"`).remove();
+            document.querySelector(`.tr.rpa-info[data-ref="${attr}"`).remove();
         }
     }
 
     for (let rpa of rpaData.entries()) {
-        if (document.querySelector(`div.rpa-info[data-ref="${rpa[0]}"`)) {
+        if (document.querySelector(`.tr.rpa-info[data-ref="${rpa[0]}"`)) {
             continue;
         }
 
         const template = document.querySelector('template.rpa-info').content.cloneNode(true);
-        const div = template.querySelector('div.rpa-info');
+        const tr = template.querySelector('.tr.rpa-info');
 
-        div.setAttribute('data-ref', rpa[0]);
-        const engine = div.querySelector('div.engine p');
-        const hostname = div.querySelector('div.hostname p');
-        const env = div.querySelector('div.env p');
-        const instance = div.querySelector('div.instance p');
-        const flowId = div.querySelector('div.name p');
-        const teanantId = div.querySelector('div.extra p');
+        tr.setAttribute('data-ref', rpa[0]);
+        const engine = tr.querySelector('.td.engine');
+        const hostname = tr.querySelector('.td.hostname');
+        const env = tr.querySelector('.td.env');
+        const instance = tr.querySelector('.td.instance');
+        const flowId = tr.querySelector('.td.name');
+        const teanantId = tr.querySelector('.td.extra');
 
+
+        switch (rpa[1].engine) {
+            case 'Power Automate':
+                engine.classList.add('pad');
+                break;
+            case 'ProcessRobot':
+                engine.classList.add('pr');
+                break;
+        }
 
         engine.innerText = rpa[1].engine;
         hostname.innerText = rpa[1].computer;
@@ -45,9 +56,13 @@ const buildRpaConvas = async () => {
         if (rpa[1]?.azureData) {
             flowId.innerText = rpa[1].azureData.flowId;
             teanantId.innerText = rpa[1].azureData.tenantId;
+
+            if (engine.classList.contains('pad')) {
+                instance.innerHTML = `<a target="_blank" href="https://make.powerautomate.com/environments/${env.innerText}/uiflows/${flowId.innerText}/runs/${instance.innerText}">${instance.innerText}</a>`;
+            }
         }
 
-        rpaView.appendChild(div);
+        rpaView.appendChild(tr);
     }
 }
 
@@ -71,10 +86,33 @@ const iteratorIncludes = (item, iter) => {
 }
 
 
-setInterval(() => {
-    buildRpaConvas().catch((err) => {
+const timer = setInterval(() => {
+    buildRpaConvas(true).catch((err) => {
         clearCanvas();
         console.error(err);
     })
 }, timeoutSeconds * 1000);
 buildRpaConvas();
+
+globalThis.clearTimer = (really) => {
+    if (really === 'Really') {
+        clearInterval(timer);
+    }
+}
+
+globalThis.insertTestData = (times) => {
+    for (let i = 0; i < times; i++) {
+        rpaData.set('b415296d-aea8-48d9-aea9-053d77450f2b' + i, {
+            engine: 'Power Automate',
+            computer: 'TESTMACHINE',
+            env: 'b415296d-aea8-48d9-aea9-053d77450f2b',
+            instance: 'b415296d-aea8-48d9-aea9-053d77450f2b',
+            azureData: {
+                flowId: 'b415296d-aea8-48d9-aea9-053d77450f2b',
+                tenantId: 'b415296d-aea8-48d9-aea9-053d77450f2b'
+            }
+        });
+    }
+
+    buildRpaConvas(false);
+}
